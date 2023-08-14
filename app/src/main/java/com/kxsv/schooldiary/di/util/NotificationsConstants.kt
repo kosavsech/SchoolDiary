2cea6c0e0d9e8648d539f9a0bd69d228addd3a7a
kosavsech <GOBLYAKIN@yandex.com>
2023-08-14 12:07:48 +0300

    added one-time requests instead of boilerplate in main, subjects, tasks, grades screens added foreground info notifications refactoring

--- app/src/main/java/com/kxsv/schooldiary/di/util/NotificationsConstants.kt
+++ app/src/main/java/com/kxsv/schooldiary/di/util/NotificationsConstants.kt
@@ -5,15 +5,12 @@ object NotificationsConstants {
 	
 	const val GRADE_CHANNEL_ID = "com.kxsv.schooldiary.GRADE_CHANNEL_ID"
 	const val FETCHED_GRADES_GROUP_ID = "com.kxsv.schooldiary.FETCHED_GRADES_GROUP_ID"
-	const val FETCHED_GRADES_SUMMARY_ID = "com.kxsv.schooldiary.FETCHED_GRADES_SUMMARY_ID"
 	
 	const val TASK_CHANNEL_ID = "com.kxsv.schooldiary.TASK_CHANNEL_ID"
 	const val FETCHED_TASKS_GROUP_ID = "com.kxsv.schooldiary.FETCHED_TASKS_GROUP_ID"
-	const val FETCHED_TASKS_SUMMARY_ID = "com.kxsv.schooldiary.FETCHED_TASKS_SUMMARY_ID"
 	
 	const val SCHEDULE_CHANNEL_ID = "com.kxsv.schooldiary.SCHEDULE_CHANNEL_ID"
 	const val FETCHED_SCHEDULE_GROUP_ID = "com.kxsv.schooldiary.FETCHED_SCHEDULE_GROUP_ID"
-	const val FETCHED_SCHEDULE_SUMMARY_ID = "com.kxsv.schooldiary.FETCHED_SCHEDULE_SUMMARY_ID"
 	
 	
 }
