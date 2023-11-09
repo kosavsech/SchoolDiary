4c6125402bcc67707c1a6963d040472c66049424
kosavsech <GOBLYAKIN@yandex.com>
2023-11-09 20:17:04 +0300

    update dialog now scrollable

--- app/src/main/java/com/kxsv/schooldiary/ui/screens/update_dialog/UpdateDialogContent.kt
+++ app/src/main/java/com/kxsv/schooldiary/ui/screens/update_dialog/UpdateDialogContent.kt
@@ -9,22 +9,30 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.kxsv.schooldiary.BuildConfig
 import com.kxsv.schooldiary.R
+import com.kxsv.schooldiary.ui.theme.AppTheme
+import com.kxsv.schooldiary.ui.util.DismissibleDialog
 import com.kxsv.schooldiary.ui.util.Indicator
 import com.pouyaheydari.appupdater.core.pojo.DialogStates
 import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.customView
-import com.vanpra.composematerialdialogs.message
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import com.vanpra.composematerialdialogs.title
 
@@ -62,19 +70,22 @@ internal fun UpdateDialogContent(
 				onDispose = {}
 			)
 			title(res = title)
-			message(text = releaseNotes)
 			customView {
-				Column {
+				Column(
+					modifier = Modifier.verticalScroll(rememberScrollState())
+				) {
 					Text(
-						text = stringResource(
-							R.string.current_version,
-							BuildConfig.VERSION_NAME
-						),
+						text = stringResource(R.string.current_version, BuildConfig.VERSION_NAME),
 						style = MaterialTheme.typography.bodyMedium
 					)
 					Text(
 						text = stringResource(R.string.update_version, version),
 						style = MaterialTheme.typography.bodyMedium
+					)
+					Text(
+						text = releaseNotes,
+						color = MaterialTheme.colorScheme.onSurface,
+						style = MaterialTheme.typography.bodyLarge,
 					)
 					if (dialogState is DialogStates.ShowUpdateInProgress) {
 						Spacer(modifier = Modifier.padding(vertical = dimensionResource(R.dimen.list_item_padding)))
@@ -104,6 +115,25 @@ internal fun UpdateDialogContent(
 					}
 				}
 			}
+		}
+	}
+}
+
+@Preview
+@Composable
+fun UpdateDialogContentPreview() {
+	AppTheme {
+		Surface {
+			val dialogProperties by remember { mutableStateOf(DismissibleDialog.properties) }
+			UpdateDialogContent(
+				dialogProperties = dialogProperties,
+				dialogState = DialogStates.ShowUpdateInProgress,
+				onDismissClick = {},
+				onUpdateClick = {},
+				releaseNotes = "Fixed:\norder of recent grades in subject detailed\nempty line of cabinet of lesson in schedule screen\nnow fixed width of subject column in report card screen\nnow fixed width of subject column in grades\ncabinet distinction on main screen, on schedule screen\nstatus bar color on light theme glitch\ncolor of weekline in schedule screen, also adjusted height of this line\nremoved year from dates in notifications\nmore intelligent new fetched task notification shortening\n\nAdded:\nswitcher to bad grades estimated(ruin of current / or lower bound)\ngrade avg predict\ncalculation of days left\ncalculation of lessons left\nability to go to subject details from lesson detailed in schedule screen ALSO swap EDIT DELETE buttons\nability to go to subject details from grade detailed\nability to create subject right from subject selection menu\nteachers screen ui improved\n",
+				version = "0.2.1-alpha",
+				title = R.string.new_version_found
+			)
 		}
 	}
 }
